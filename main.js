c44f092cc28c050aefa0dbf8d7bd8a06cf527a59
Delarychi <85495891+Delarychi@users.noreply.github.com>
2024-01-11 19:50:51 +0300

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const HOLIDAYS = [
 ]; 
 
 const orderModal = document.getElementById('orderModal');
-const dateField = orderModal.querySelector('#orderDate');
+const dateField = orderModal.querySelector('#date');
 const timeField = orderModal.querySelector('#startTime');
 const durationField = orderModal.querySelector('#orderDuration');
 const personsField = orderModal.querySelector('#personsCount');
@@ -22,25 +22,6 @@ const priceField = orderModal.querySelector('#price');
 const studentField = orderModal.querySelector('#isStudent');
 const transportField = orderModal.querySelector('#isTransport');
 
-function clearRoutesTable() {
-    document.getElementById('routesTableBody').innerHTML = '';
-}
-
-function addRoutesToTable(routes) {
-    const tableBody = document.getElementById('routesTableBody');
-  
-    routes.forEach(route => {
-        const row = tableBody.insertRow();
-        row.insertCell(0).innerHTML = route.name;
-        row.insertCell(1).innerHTML = route.description;
-        row.insertCell(2).innerHTML = route.mainObject;
-
-        const selectButton = document.createElement('button');
-        selectButton.innerText = 'Выбрать';
-        selectButton.addEventListener('click', () => guideDownload(route));
-        row.insertCell(3).appendChild(selectButton);
-    });
-}
 
 function isThisDayOff(dateString) { // высчитывает день недели
     let date = new Date(dateString);
@@ -68,33 +49,17 @@ function getTimeExtra(startTime) { // высчитывает время нача
     return 0;
 }
 
-function calculateCost(guideCost, duration, // высчитывает общую сумму
-    date, startTime, 
-    personsNumber, students, transport
-) {
+function calculateCost(guideCost, duration, date, startTime, 
+    personsNumber, students, transport) { // высчитывает общую сумму
     let price = guideCost * duration * isThisDayOff(date);
-    if (personsNumber > 10) {
-        price += 1500;
-    } else if ((personsNumber > 5) && (personsNumber <= 10)) {
-        price += 1000;
-    }
-
-    if (transport && isThisDayOff(date) == 1.5) {
-        price *= 1.25;
-    } else {
-        price *= 1.3;
-    }
-
-    if (students) {
-        price *= 0.85;
-    }
-
+    price += personsNumber > 5 && personsNumber <= 10 ? 1000 : 0;
+    price += personsNumber > 10 && personsNumber <= 20 ? 1500 : 0;
+    price *= transport &&  (isThisDayOff(date) == 1.5) ? 1.25 : 1.3;
+    price *= students ? 0.85 : 1;
     return Math.floor(price);
 }
 
 function calculateOrderCost() { // выводит общую сумму
-    console.log("calculationg");
-
     const date = dateField.value;
     const time = timeField.value;
     const duration = durationField.value;
@@ -106,12 +71,189 @@ function calculateOrderCost() { // выводит общую сумму
     const cost = calculateCost(orderModal.guide.pricePerHour, duration, date,
         time, persons, student, transport);
 
-    priceField.textContent = cost + ' руб.';
+    priceField.textContent = cost + ' рублей.';
                                   
     return cost;
 }
 
-function setModalWindow(route, guide) { // выводит окно заявки для просмотра(переделать, много костылей)
+function clearRoutesTable() {// очистка таблицы с маршрутами
+    document.getElementById('routesTableBody').innerHTML = '';
+}
+
+function addRoutesToTable(routes) { // загрузка таблицы с маршрутами
+    const tableBody = document.getElementById('routesTableBody');
+  
+    routes.forEach(route => {
+        const row = tableBody.insertRow();
+        row.insertCell(0).innerHTML = route.name;
+        row.insertCell(1).innerHTML = route.description;
+        row.insertCell(2).innerHTML = route.mainObject;
+
+        const selectButton = document.createElement('button');
+        selectButton.innerText = 'Выбрать';
+        selectButton.addEventListener('click', () => guideDownload(route));
+        row.insertCell(3).appendChild(selectButton);
+    });
+}
+
+function highlightSearchResult(searchKeyword) {// поиск и выделение в маршрутах
+    const tableBody = document.getElementById('routesTableBody');
+    const rows = tableBody.getElementsByTagName('tr');
+  
+    for (let i = 0; i < rows.length; i++) {
+        const cells = rows[i].getElementsByTagName('td');
+        const nameCell = cells[0];
+
+        const cellValue = nameCell.innerText;
+
+        const lowerCaseCellValue = cellValue.toLowerCase();
+        const lowerCaseSearchKeyword = searchKeyword.toLowerCase();
+  
+        if (lowerCaseCellValue.includes(lowerCaseSearchKeyword)) {
+            const startIndex = lowerCaseCellValue.indexOf(
+                lowerCaseSearchKeyword
+            );
+
+            const endIndex = startIndex + searchKeyword.length;
+  
+            const highlightedText = cellValue.substring(0, startIndex) +
+          `<span class="search-highlight">${cellValue.substring(startIndex, endIndex)}</span>` +
+            cellValue.substring(endIndex);
+  
+            nameCell.innerHTML = highlightedText;
+        }
+    }
+}
+
+function searchRoutes() {// поиск и выделение в маршрутах на основе чекбокса и строки в таблице маршрутов(пока нигде не вызывается)
+    const searchKeyword = document.getElementById('routeNameInput')
+        .value.toLowerCase();
+    const selectedObject = document.getElementById('mainObjectSelect')
+        .value.toLowerCase();
+  
+    filteredRoutes = routesData.filter(route => 
+        route.name.toLowerCase().includes(searchKeyword) &&
+        route.mainObject.toLowerCase().includes(selectedObject)
+    );
+  
+    const limitedRoutes = filteredRoutes.slice(0, itemsPerPage);
+  
+    clearRoutesTable();
+    addRoutesToTable(limitedRoutes);
+    updatePaginationAfterSearch(filteredRoutes);
+    highlightSearchResult(searchKeyword);
+}
+  
+function resetSearch() {// сброс параметров поиска таблицы маршрутов(пока нигде не вызывается)
+    document.getElementById('routeNameInput').value = '';
+    document.getElementById('mainObjectSelect').value = '';
+    filteredRoutes = null;
+    updateTable();
+}
+
+function getoptionforselect(q) {// удаляет из массива все повторяющиеся переменные
+    return [... new Set(q)];    
+}
+
+function clearLanguageOptions(selectElement) {// отвечает за чекбокс с языками экскурсии
+    const select = document.getElementById('selectedLanguge');
+    select.innerHTML = '';
+
+    const option = document.createElement('option');
+    option.value = "";
+    option.innerHTML = "Язык экскурсии";
+    select.appendChild(option);
+}
+
+function updateLanguageSelect(languages) {// отвечает за чекбокс с языками экскурсии
+    const select = document.getElementById('selectedLanguge');
+    for (let i in languages) {
+        const option = document.createElement('option');
+        option.value = i;
+        option.innerHTML = languages[i];
+        select.appendChild(option);
+    }  
+}
+
+function guideDownload(route) { // загрузка таблицы с гидами
+    let guideTable = document.querySelector('#guideTable');
+    let arr = [];
+    fetch(`${HOST}/api/routes/${route.id}/guides?api_key=${API_KEY}`)
+        .then(response => response.json())
+        .then(response => {
+            arr = [];
+            clearLanguageOptions();
+            guideTable.innerHTML = '';
+            for (let item of response) {
+                const row = guideTable.insertRow();
+
+                const guidePhoto = document.createElement('img');
+                guidePhoto.src = './images/profile.png';
+                guidePhoto.style.height = '34px';
+                guidePhoto.style.width = '34px';
+                guidePhoto.alt = 'Фото профиля';
+                
+                row.insertCell(0).appendChild(guidePhoto);
+                row.insertCell(1).textContent = item.name;
+                row.insertCell(2).textContent = item.language;
+                row.insertCell(3).textContent = item.workExperience;
+                row.insertCell(4).textContent = item.pricePerHour;
+
+                const selectButton = document.createElement('button');
+
+                selectButton.setAttribute('data-bs-toggle', 'modal');
+                selectButton.setAttribute('data-bs-target', '#orderModal');
+                selectButton.textContent = 'Выбрать';
+
+                selectButton.addEventListener('click', () => {
+                    setModalWindow(route, item);
+                });
+                row.insertCell(5).appendChild(selectButton);
+
+                const fromExperiense = document.getElementById('guideFromExperiense').value;
+
+                const toExperiense = document.getElementById('guideToExperiense').value;
+
+                const selectedLanguage = document.getElementById('selectedLanguge').value;
+
+                if ((fromExperiense !== '') &&
+                    (fromExperiense > item.workExperience) ||
+                    (toExperiense !== '') &&
+                    (toExperiense < item.workExperience) &&
+                    selectedLanguage === item.language) {
+                    row.classList.add("none");
+                }
+                guideTable.append(row);
+                arr.push(item.language);
+            } 
+            updateLanguageSelect(getoptionforselect(arr));
+        });
+}
+
+function guideOptions() {// фильтрует таблицу гидов
+    let list = document.querySelectorAll('#guideTable tr');
+    let from = Number(document.getElementById('guideFromExperiense').value);
+    let to = Number(document.getElementById('guideToExperiense').value);
+
+    const selectedLanguage = document.getElementById('selectedLanguge');
+    for (let i in list) {
+        if ((from == 0 || from <= list[i].cells[3].innerHTML) &&
+        (to == 0 || to >= list[i].cells[3].innerHTML) &&
+        (selectedLanguage)
+            .options[selectedLanguage.selectedIndex]
+            .innerHTML === 'Язык экскурсии' ||
+        (selectedLanguage)
+            .options[selectedLanguage.selectedIndex]
+            .innerHTML === list[i].cells[2].innerHTML) {
+
+            list[i].classList.remove("none");
+        } else {
+            list[i].classList.add("none");
+        }
+    }
+}
+
+function setModalWindow(route, guide) {// установка окна заявки
     orderModal.guide = guide;
 
     orderModal.querySelector('#routeName').value = route.name;
@@ -151,91 +293,7 @@ function setModalWindow(route, guide) { // выводит окно заявки 
     };
 }
 
-function getoptionforselect(q) {
-    return [... new Set(q)];    
-}
-
-function clearLanguageOptions(selectElement) {
-    const select = document.getElementById('selectedLanguge');
-    select.innerHTML = '';
-
-    const option = document.createElement('option');
-    option.value = "";
-    option.innerHTML = "Язык экскурсии";
-    select.appendChild(option);
-}
-
-function updateLanguageSelect(languages) {
-    const select = document.getElementById('selectedLanguge');
-    for (let i in languages) {
-        const option = document.createElement('option');
-        option.value = i;
-        option.innerHTML = languages[i];
-        select.appendChild(option);
-    }  
-}
-
-function guideDownload(route) {
-    let guideTable = document.querySelector('#guideTable');
-    let arroption = [];
-    fetch(`${HOST}/api/routes/${route.id}/guides?api_key=${API_KEY}`)
-        .then(response => response.json())
-        .then(response => {
-            arroption = [];
-            clearLanguageOptions();
-            guideTable.innerHTML = '';
-            for (let item of response) {
-                const row = guideTable.insertRow();
-
-                const guidePhoto = document.createElement('img');
-                guidePhoto.src = '';// решить проблему с фото
-                guidePhoto.alt = 'Фото профиля';
- 
-                row.insertCell(0).appendChild(guidePhoto);
-                row.insertCell(1).textContent = item.name;
-                row.insertCell(2).textContent = item.language;
-                row.insertCell(3).textContent = item.workExperience;
-                row.insertCell(4).textContent = item.pricePerHour;
-
-                const selectButton = document.createElement('button');
-
-                selectButton.setAttribute('data-bs-toggle', 'modal');
-                selectButton.setAttribute('data-bs-target', '#orderModal');
-                selectButton.textContent = 'Выбрать';
-
-                selectButton.addEventListener('click', () => {
-                    setModalWindow(route, item);
-                });
-
-                row.insertCell(5).appendChild(selectButton);
-
-                const fromExperiense = document.getElementById(
-                    'guideFromExperiense'
-                ).value;
-
-                const toExperiense = document.getElementById(
-                    'guideToExperiense'
-                ).value;
-
-                const selectedLanguage = document.getElementById(
-                    'selectedLanguge'
-                ).value;
-
-                if ((fromExperiense !== '') &&
-                    (fromExperiense > item.workExperience) ||
-                    (toExperiense !== '') &&
-                    (toExperiense < item.workExperience) &&
-                    selectedLanguage === item.language) {
-                    row.classList.add("d-none");
-                }
-                guideTable.append(row);
-                arroption.push(item.language);
-            } 
-            updateLanguageSelect(getoptionforselect(arroption));
-        });
-}
-  
-function fetchRoutesFromApi() {
+function fetchRoutesFromApi() {// получает данные из api о маршрутах
     fetch(
         `${HOST}/api/routes?api_key=${API_KEY}`
     )
@@ -247,7 +305,7 @@ function fetchRoutesFromApi() {
         .catch(error => console.error('Error fetching route data:', error));
 }
 
-function updateTable() {
+function updateTable() { // управляет обновлением и отображением данных о маршрутах
     const startIndex = (currentPage - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
     const currentRoutes = filteredRoutes ?
@@ -266,7 +324,7 @@ function updateTable() {
     }
 }
   
-function handlePageClick(pageNumber) {
+function handlePageClick(pageNumber) {// вызывает разные значения в соответствии со страницей
     currentPage = pageNumber;
     updateTable();
 }
@@ -304,7 +362,7 @@ function createPaginationItem(text, pageNumber) {
     return pageItem;
 }
 
-function updatePaginationAfterSearch(filteredRoutes) {
+function updatePaginationAfterSearch(filteredRoutes) {// обновляет элементы пагинации на основе фильтрации
     const paginationElement = document.getElementById('pagination');
     const totalPages = Math.ceil(filteredRoutes.length / itemsPerPage);
   
@@ -322,7 +380,7 @@ function updatePaginationAfterSearch(filteredRoutes) {
     paginationElement.appendChild(nextItem);
 }
   
-function updatePagination() {
+function updatePagination() {// обновлении таблицы на основе пагинации
     const paginationElement = document.getElementById('pagination');
     const totalPages = Math.ceil((filteredRoutes ?
         filteredRoutes.length : 
@@ -342,85 +400,7 @@ function updatePagination() {
     paginationElement.appendChild(nextItem);
 }  
 
-function highlightSearchResult(searchKeyword) {
-    const tableBody = document.getElementById('routesTableBody');
-    const rows = tableBody.getElementsByTagName('tr');
-  
-    for (let i = 0; i < rows.length; i++) {
-        const cells = rows[i].getElementsByTagName('td');
-        const nameCell = cells[0];
-
-        const cellValue = nameCell.innerText;
-
-        const lowerCaseCellValue = cellValue.toLowerCase();
-        const lowerCaseSearchKeyword = searchKeyword.toLowerCase();
-  
-        if (lowerCaseCellValue.includes(lowerCaseSearchKeyword)) {
-            const startIndex = lowerCaseCellValue.indexOf(
-                lowerCaseSearchKeyword
-            );
-
-            const endIndex = startIndex + searchKeyword.length;
-  
-            const highlightedText = cellValue.substring(0, startIndex) +
-          `<span class="search-highlight">${cellValue.substring(startIndex, endIndex)}</span>` +
-            cellValue.substring(endIndex);
-  
-            nameCell.innerHTML = highlightedText;
-        }
-    }
-}
-
-function searchRoutes() {
-    const searchKeyword = document.getElementById('routeNameInput')
-        .value.toLowerCase();
-    const selectedObject = document.getElementById('mainObjectSelect')
-        .value.toLowerCase();
-  
-    filteredRoutes = routesData.filter(route => 
-        route.name.toLowerCase().includes(searchKeyword) &&
-        route.mainObject.toLowerCase().includes(selectedObject)
-    );
-  
-    const limitedRoutes = filteredRoutes.slice(0, itemsPerPage);
-  
-    clearRoutesTable();
-    addRoutesToTable(limitedRoutes);
-    updatePaginationAfterSearch(filteredRoutes);
-    highlightSearchResult(searchKeyword);
-}
-  
-function resetSearch() {
-    document.getElementById('routeNameInput').value = '';
-    document.getElementById('mainObjectSelect').value = '';
-    filteredRoutes = null;
-    updateTable();
-}
-
-function guideOptions() {
-    let list = document.querySelectorAll('#guideTable tr');
-    let from = Number(document.getElementById('guideFromExperiense').value);
-    let to = Number(document.getElementById('guideToExperiense').value);
-
-    const selectedLanguage = document.getElementById('selectedLanguge');
-    for (let i in list) {
-        if ((from == 0 || from <= list[i].cells[3].innerHTML) &&
-        (to == 0 || to >= list[i].cells[3].innerHTML) &&
-        (selectedLanguage)
-            .options[selectedLanguage.selectedIndex]
-            .innerHTML === 'Язык экскурсии' ||
-        (selectedLanguage)
-            .options[selectedLanguage.selectedIndex]
-            .innerHTML === list[i].cells[2].innerHTML) {
-
-            list[i].classList.remove("d-none");
-        } else {
-            list[i].classList.add("d-none");
-        }
-    }
-}
-
-function clickHandler(event) {
+function clickHandler(event) {// обработка событий
     const screen = document.querySelector('.screen');
     const target = event.target;
     const row = document.querySelectorAll('th');
